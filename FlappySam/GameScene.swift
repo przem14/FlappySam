8635496414b903380e169dc2f57cf1d29a0d12c8
przem14 <przem.wawrzyniak@gmail.com>
2016-03-07 20:09:07 +0100

    First commit

---
+++ FlappySam/GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  FlappySam
+//
+//  Created by Przemysław Wawrzyniak on 07.03.2016.
+//  Copyright (c) 2016 Przemyslaw Wawrzyniak. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+    }
+}
---
+++ FlappySam/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GameViewController.swift
+//  FlappySam
+//
+//  Created by Przemysław Wawrzyniak on 07.03.2016.
+//  Copyright (c) 2016 Przemyslaw Wawrzyniak. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let scene = GameScene(fileNamed:"GameScene") {
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.ignoresSiblingOrder = true
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return .AllButUpsideDown
+        } else {
+            return .All
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
